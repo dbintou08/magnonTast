4f1e502eb8bc231532a4361d4848174fa55249a2
dpro <dbintou008@gmail.com>
2023-06-21 12:18:01 +0000

    test2

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,19 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe 'Task management function', type: :system do
+RSpec.describe 'Fonction d\'affichage de liste', type: :system do
   describe 'Registration function' do
-    context 'When registering a task' do
-      it 'The registered task is displayed' do
-        visit new_task_path
-        fill_in "Titre", with: "title"
-        fill_in "content", with: "content"
-        click_button "Create Task"
-        expect(page).to have_content "Task was successfully created."
-      end
-    end
+   context 'When registering a task' do
+     it 'The registered task is displayed' do
+       visit new_task_path
+       fill_in "Titre", with: "first_task"
+       fill_in "contenu", with: "rangements des taches"
+       click_button "Creer"
+       expect(page).to have_content "rangements des taches"
+     end
+   end
   end
-
-  describe 'List display function' do
+   describe 'Fonction d\'affichage de liste' do
     let!(:first_task) { FactoryBot.create(:task, titre: "first_task", created_at: Time.zone.now.ago(3.days)) }
     let!(:second_task) { FactoryBot.create(:task, titre: "second_task", created_at: Time.zone.now.ago(2.days)) }
     let!(:third_task) { FactoryBot.create(:task, titre: "third_task", created_at: Time.zone.now.ago(1.days)) }
@@ -32,10 +31,23 @@ RSpec.describe 'Task management function', type: :system do
       context 'When creating a new task' do
         it 'New task is displayed at the top' do
         visit new_task_path
-        fill_in "Titre", with: "title1"
-        fill_in "contenu", with: "content"
+        fill_in "Titre", with: "first_task"
+        fill_in "contenu", with: "Test est ok"
         click_button "Creer"
-        expect(page).to have_content "title1"
+        expect(page).to have_content "first_task"
         end
       end
     end
+    describe 'Detailed display function' do
+
+      context 'When transitioned to any task details screen' do
+         let (:task) {FactoryBot.create(:task, titre: 'Test', content: 'Je suis un contenu')}
+         it 'The content of the task is displayed' do
+ 
+           visit task_path(task)
+           visit task_path(task)
+           expect(page).to have_content 'Je suis un contenu'
+         end
+      end
+   end
+ end
